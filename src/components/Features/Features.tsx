85479eb8cf2f10bdea3fc8604ebe0df3d4d2c7ea
bimbo-ade <bimboadeyemi65@gmail.com>
2023-01-12 11:18:17 +0100

    style: added icons and styling to navbar component

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -1,29 +1,35 @@
 import "./Features.scss";
+import acUser from "../../assets/images/acUser.png";
 
+import loUser from "../../assets/images/loUser.png";
+
+import saUser from "../../assets/images/saUser.png";
+
+import user from "../../assets/images/user.png";
 const Features = () => {
   return (
     <>
       <h1>Users</h1>
       <div className="features-container">
-        <div className="div1">
-          icon
-          <h5>Total Balance</h5>
-          <h2>N 1222</h2>
+        <div className="card">
+          <img src={user} alt="user" width={40} />
+          <h5>USER</h5>
+          <h2>2,453</h2>
         </div>
-        <div className="dive2">
-          icon
-          <h5> Naira</h5>
-          <h2>N 402 000. 00</h2>
+        <div className="card">
+          <img src={acUser} alt="active" width={40} />
+          <h5> ACTIVE USERS</h5>
+          <h2>2,453</h2>
         </div>
-        <div className="div3">
-          icon
-          <h5>$ USDT</h5>
-          <h2>N 402 000. 00</h2>
+        <div className="card">
+          <img src={loUser} alt="loans" width={40} />
+          <h5>USERS WITH LOANS</h5>
+          <h2>12,453</h2>
         </div>
-        <div className="div4">
-          icon
-          <h5>$ USDT</h5>
-          <h2>N 402 000. 00</h2>
+        <div className="card">
+          <img src={saUser} alt="savings" width={40} />
+          <h5>USERS WITH SAVINGS</h5>
+          <h2>102,543 </h2>
         </div>
       </div>{" "}
     </>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,13 @@
 import "./Navbar.scss";
 import logo from "../../assets/images/logo.png";
+
+import search from "../../assets/images/search.png";
+
+import dropdown from "../../assets/images/dropdown.png";
+
+import bell from "../../assets/images/bell.png";
+
+import avatar from "../../assets/images/avatar.png";
 const Navbar = () => {
   return (
     <div className="nav">
@@ -7,16 +15,22 @@ const Navbar = () => {
       <div className=" search-cont">
         <img src={logo} alt="logo" width={140} />
         <div className="search">
-          <input type="text" placeholder="search" className="input" />
-          <div className="icon">icon</div>
+          <input
+            type="text"
+            placeholder="search for anything"
+            className="input"
+          />
+          <div className="icon">
+            <img src={search} alt="" width={20} />
+          </div>
         </div>
       </div>
       <div className="user-info">
         <p>Docs</p>
-        icon
-        <img src="" alt="" />
+        <img src={bell} alt="" width={20} />
+        <img src={avatar} alt="" width={20} />
         <span>Adedeji</span>
-        icon
+        <img src={dropdown} alt="" width={20} />
       </div>
     </div>
   );
